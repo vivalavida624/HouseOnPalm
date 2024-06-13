f53e0f9bb0ab36c9620eb77eb8bf00ac67d8f1eb
alfredmaomao <alfredmaomao@hotmail.com>
2024-06-12 20:49:56 -0400

    broker image placeholder add
    broker image placeholder add

--- app/src/main/java/com/example/houseonplam/ui/brokers/BrokerDetailFragment.kt
+++ app/src/main/java/com/example/houseonplam/ui/brokers/BrokerDetailFragment.kt
@@ -5,14 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.navArgs
 import com.example.houseonplam.databinding.FragmentBrokerDetailBinding
 
 class BrokerDetailFragment : Fragment() {
 
     private var _binding: FragmentBrokerDetailBinding? = null
     private val binding get() = _binding!!
-    private val args: BrokerDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,16 +20,12 @@ class BrokerDetailFragment : Fragment() {
         val root: View = binding.root
 
         // 从Safe Args中获取数据
-        val brokerName = args.brokerName
-        val brokerTitle = args.brokerTitle
+        val brokerName = arguments?.getString("brokerName") ?: "Unknown"
+        val brokerTitle = arguments?.getString("brokerTitle") ?: "Unknown"
 
         // 绑定数据到视图
         binding.brokerNameTextView.text = brokerName
         binding.brokerTitleTextView.text = brokerTitle
-        binding.marketListings.text = "On Market Listings"
-        binding.informationRequest.text = "Information Request"
-        binding.contact.text = "Contact"
-        binding.email.text = "Email"
 
         return root
     }
