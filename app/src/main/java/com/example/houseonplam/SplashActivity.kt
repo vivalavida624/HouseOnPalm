81ae7d4a17a2bc42177582490c262f6f8659fef0
alfredmaomao <alfredmaomao@hotmail.com>
2024-06-13 22:54:55 -0400

    add Splash Screen
    add Splash Screen

---
+++ app/src/main/java/com/example/houseonplam/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.houseonplam
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // 延迟启动主活动
+        val splashScreenDuration = 2000 // 启动画面显示时间 (毫秒)
+        val intent = Intent(this, MainActivity::class.java)
+        window.decorView.postDelayed({
+            startActivity(intent)
+            finish()
+        }, splashScreenDuration.toLong())
+    }
+}
